f2b487333900234d8ff3114bad7ff63b525c2c8a
Adharsh <adharshkrish@outlook.com>
2020-01-26 13:24:01 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+apiKey: "AIzaSyC5ZH2s7eGof2Dk6wOWe0APbdAcjq4sxnc",
+authDomain: "pec-promoclub.firebaseapp.com",
+databaseURL: "https://pec-promoclub.firebaseio.com",
+projectId: "pec-promoclub",
+storageBucket: "pec-promoclub.appspot.com",
+messagingSenderId: "771915701365",
+appId: "1:771915701365:web:a27c09fd55344ed18cec3c",
+measurementId: "G-MPL29PDR1K"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
