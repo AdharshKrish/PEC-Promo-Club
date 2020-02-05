a9cd7005926578e4357891516c2f67eb1b454578
Adharsh <adharshkrish@outlook.com>
2020-02-05 16:22:31 +0530

    made it exclusive to pec

--- public/app.js
+++ public/app.js
@@ -8,70 +8,61 @@ messagingSenderId: "771915701365",
 appId: "1:771915701365:web:a27c09fd55344ed18cec3c",
 measurementId: "G-MPL29PDR1K"
 };
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+
 let UID;
-function toggleSignIn() {
-  if (firebase.auth().currentUser) {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
-  } else {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    // if (email.length < 4) {
-    //   alert('Please enter an email address.');
-    //   return;
-    // }
-    // if (password.length < 4) {
-    //   alert('Please enter a password.');
-    //   return;
-    // }
-    // Sign in with email and pass.
-    // [START authwithemail]
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // [START_EXCLUDE]
-      if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-      document.getElementById('quickstart-sign-in').disabled = false;
-      // [END_EXCLUDE]
-    });
-    // [END authwithemail]
+
+function signIn(){
+  var email = inemail.value;
+  var password = inpass.value;
+  if (email.length < 4 || !email.includes("@pec.edu")) {
+    alert('Please enter your pec.edu email address');
+    return;
   }
-  document.getElementById('quickstart-sign-in').disabled = true;
+  if (password.length < 4) {
+    alert('Please enter a password');
+    return;
+  }
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong password.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+  
+  firebase.auth().onAuthStateChanged(function (user){
+    if(user){
+      if(user.emailVerified){
+          login.style.display="none";
+          home.style.display="block";
+          // let usr = firebase.auth().currentUser;
+          dispname.innerHTML=user.displayName;
+          UID = user.uid;
+          console.log(UID);
+      }
+      else{
+        signOut();
+        alert("Please verify your email and then continue");
+        sendEmailVerification();
+      }
+    }
+  });
 }
 
-/**
- * Handles the sign up button press.
- */
 function handleSignUp() {
   var email = document.getElementById('upemail').value;
   var password = document.getElementById('uppassword').value;
-  // if (email.length < 4) {
-  //   alert('Please enter an email address.');
-  //   return;
-  // }
-  // if (password.length < 4) {
-  //   alert('Please enter a password.');
-  //   return;
-  // }
-  // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   if (errorCode == 'auth/weak-password') {
-  //     alert('The password is too weak.');
-  //   } else {
-  //     alert(errorMessage);
-  //   }
-  //   console.log(error);
-  // });
+  if (email.length < 4 || !email.includes("@pec.edu")) {
+    alert('Please enter your pec.edu email address');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function (result) {
@@ -80,111 +71,55 @@ function handleSignUp() {
       })
     }).catch(function (error) {
       console.log(error);
-      // var errorCode = error.code;
     var errorMessage = error.message;
     if (errorCode == 'auth/weak-password') {
       alert('The password is too weak.');
     } else {
       alert(errorMessage);
     }
-    // console.log(error);
     });
-  // [END createwithemail]
+    // signOut();
+    // alert("hi");
+    firebase.auth().onAuthStateChanged(function (user){
+      if(user){
+        signOut();
+        alert("Account created and verification email sent! Please verify your email then sign in");
+      }});  
 }
 
-/**
- * Sends an email verification to the user.
- */
-function sendEmailVerification() {
-  // [START sendemailverification]
-  firebase.auth().currentUser.sendEmailVerification().then(function() {
-    // Email Verification sent!
-    // [START_EXCLUDE]
-    alert('Email Verification Sent!');
-    // [END_EXCLUDE]
-  });
-  // [END sendemailverification]
+function signOut() {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut(); 
+      login.style.display = "block";
+    home.style.display = "none";
+  }
 }
-
-function sendPasswordReset() {
-  var email = document.getElementById('email').value;
-  // [START sendpasswordemail]
-  firebase.auth().sendPasswordResetEmail(email).then(function() {
-    // Password Reset Email Sent!
-    // [START_EXCLUDE]
+function passReset(){
+  var email = inemail.value;
+    alert('Please wait');
+  firebase.auth().sendPasswordResetEmail(email).then(function () {
     alert('Password Reset Email Sent!');
-    // [END_EXCLUDE]
-  }).catch(function(error) {
-    // Handle Errors here.
+  }).catch(function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // [START_EXCLUDE]
     if (errorCode == 'auth/invalid-email') {
       alert(errorMessage);
     } else if (errorCode == 'auth/user-not-found') {
       alert(errorMessage);
     }
     console.log(error);
-    // [END_EXCLUDE]
   });
-  // [END sendpasswordemail];
 }
 
-/**
- * initApp handles setting up UI event listeners and registering Firebase auth listeners:
- *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
- *    out, and that is where we update the UI.
- */
-// initApp();
-function initApp() {
-  // Listening for auth state changes.
-  // [START authstatelistener]
-  firebase.auth().onAuthStateChanged(function(user) {
-    // [START_EXCLUDE silent]
-    document.getElementById('quickstart-verify-email').disabled = true;
-    // [END_EXCLUDE]
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // [START_EXCLUDE]
-      document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-      document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-      document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-      if (!emailVerified) {
-        document.getElementById('quickstart-verify-email').disabled = false;
-      }
-      // [END_EXCLUDE]
-    } else {
-      // User is signed out.
-      // [START_EXCLUDE]
-      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-      document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-      document.getElementById('quickstart-account-details').textContent = 'null';
-      // [END_EXCLUDE]
-    }
-    // [START_EXCLUDE silent]
-    document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
+function sendEmailVerification() {
+  firebase.auth().currentUser.sendEmailVerification().then(function() {
+    alert('Email Verification Sent!');
   });
-  // [END authstatelistener]
-
-  document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-  document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-  document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-  document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
 }
-
 
 function disp(){
-
   var name, email, photoUrl, uid, emailVerified;
-
+  //firebase.auth().currentUser
   firebase.auth().onAuthStateChanged(function (user){
     name = user.displayName;
     email = user.email;
@@ -207,7 +142,6 @@ function autoLogin() {
   }
     getPost();
   });
- 
 }
 
 function addpost(){
@@ -238,35 +172,17 @@ function getPost(){
       let childData = childSnapshot.val();
       let like = JSON.stringify(childData);
 
-      // console.log(childSnapshot);
-      // console.log(childData['star']);
-      // console.log(JSON.stringify(childData['star']));
-
         if(like.includes(UID)){
           // symbol = "&#x1F497";
           // symbol="&#x2764;";
           symbol="&#x1f44d;";
           code=1;
-          // console.log("likeee");
         }
       postContainer.innerHTML += '<eachpost>' + childData['content'] + '</eachpost><button onclick=addLike("' + postKey +'",'+code+ ')>'+symbol+'</button>'+ childData['starCount']+'<br><br>';
 
     });
   });
 }
-      // console.log(childData);
-      // console.log(childData["content"]);
-
-      // var eachpost = document.createElement("eachpost");                 // Create a <p> element
-      // eachpost.innerHTML = childData["content"]+"<br>"+childData["starCount"];                // Insert text
-      // document.getElementById("postContainer").appendChild(eachpost);
-      // document.getElementById("postContainer").appendChild(document.createElement("br"));
-      // document.getElementById("postContainer").appendChild(document.createElement("br"));
-
-      // ...
-      // childSnapshot.forEach(function (childofchild){
-
-      // })
 
 function addLike(key,c){
   let database = firebase.database();
